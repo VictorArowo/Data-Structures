1f261085d0ce8840346ffedc767490c3ec6c3b49
Victor <arowovici@gmail.com>
2020-02-24 15:11:03 +0100

    Implemented Stack Data Structure

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -1,18 +1,31 @@
 import sys
-sys.path.append('../doubly_linked_list')
+sys.path.insert(
+    1, '/Users/victor/Documents/python/data_structures/doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
+
+
+# Stack => LIFO
+
 
 class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        """
+        A DLL is a good choice as we are easily able to remove items from the tail of the list since we have two-way binding.
+        If we had used a SLL, we would have no access to the element just before the tail from the tail.
+        """
+
+        self.storage = DoublyLinkedList()
 
     def push(self, value):
-        pass
+        self.size += 1
+        self.storage.add_to_tail(value)
 
     def pop(self):
-        pass
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.remove_from_tail()
 
     def len(self):
-        pass
+        return self.size
